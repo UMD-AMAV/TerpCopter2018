c0220a0bb8d12a5b2fbad704ef5298a19709478c
amahowal <amahowal@umd.edu>
2018-01-21 15:23:15 -0500

    remove build dir

--- build/rosserial/rosserial_client/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/austin/Documents/AMAV/TerpCopter2018/src/rosserial/rosserial_client
-# Build directory: /home/austin/Documents/AMAV/TerpCopter2018/build/rosserial/rosserial_client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_rosserial_client_gtest_float64_test "/home/austin/Documents/AMAV/TerpCopter2018/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/indigo/share/catkin/cmake/test/run_tests.py" "/home/austin/Documents/AMAV/TerpCopter2018/build/test_results/rosserial_client/gtest-float64_test.xml" "--return-code" "/home/austin/Documents/AMAV/TerpCopter2018/devel/lib/rosserial_client/float64_test --gtest_output=xml:/home/austin/Documents/AMAV/TerpCopter2018/build/test_results/rosserial_client/gtest-float64_test.xml")
-add_test(_ctest_rosserial_client_gtest_subscriber_test "/home/austin/Documents/AMAV/TerpCopter2018/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/indigo/share/catkin/cmake/test/run_tests.py" "/home/austin/Documents/AMAV/TerpCopter2018/build/test_results/rosserial_client/gtest-subscriber_test.xml" "--return-code" "/home/austin/Documents/AMAV/TerpCopter2018/devel/lib/rosserial_client/subscriber_test --gtest_output=xml:/home/austin/Documents/AMAV/TerpCopter2018/build/test_results/rosserial_client/gtest-subscriber_test.xml")
--- build/rosserial/rosserial_msgs/catkin_generated/rosserial_msgs-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(rosserial_msgs_MESSAGE_FILES "/home/austin/Documents/AMAV/TerpCopter2018/src/rosserial/rosserial_msgs/msg/Log.msg;/home/austin/Documents/AMAV/TerpCopter2018/src/rosserial/rosserial_msgs/msg/TopicInfo.msg")
-set(rosserial_msgs_SERVICE_FILES "/home/austin/Documents/AMAV/TerpCopter2018/src/rosserial/rosserial_msgs/srv/RequestMessageInfo.srv;/home/austin/Documents/AMAV/TerpCopter2018/src/rosserial/rosserial_msgs/srv/RequestParam.srv;/home/austin/Documents/AMAV/TerpCopter2018/src/rosserial/rosserial_msgs/srv/RequestServiceInfo.srv")
