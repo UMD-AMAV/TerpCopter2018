cc2e76620960d38b4bd98e6a34c8403fe90eb30c
imoss15 <40700833+imoss15@users.noreply.github.com>
2018-11-11 15:01:06 -0500

    hey zach

--- summer/quadcopter_UMD_12_07/behaviorManagerDebug/missionParam.m
+++ summer/quadcopter_UMD_12_07/behaviorManagerDebug/missionParam.m
@@ -14,6 +14,7 @@ mission.config.currentBhvIndex = 1;
 % Define mission behaviors
 ind = 0;
 
+%Behavior 1 (mission.bhv{1})
 ind = ind+1;
 mission.bhv{ind}.name = 'bhv_takeoff';
 mission.bhv{ind}.params.desiredAltMeters = .5;
@@ -21,12 +22,14 @@ mission.bhv{ind}.params.timeoutSec= 60.0;
 mission.bhv{ind}.completion.status = false;     % completion flag
 mission.bhv{ind}.completion.threshold = 0.1;    % tolerance
 
+%Behavior 2 (mission.bhv{2})
 ind = ind+1;
 mission.bhv{ind}.name = 'bhv_hover';
 mission.bhv{ind}.params.desiredAltMeters = 1.0;
 mission.bhv{ind}.completion.durationSec = 9.95; % 10 seconds
 mission.bhv{ind}.completion.status = false;     % completion flag
 
+%Behavior 3 (mission.bhv{3})
 ind = ind+1;
 mission.bhv{ind}.name = 'bhv_landinghover';
 mission.bhv{ind}.params.desiredAltMeters = 0.5;
@@ -34,6 +37,7 @@ mission.bhv{ind}.completion.type = 'time';
 mission.bhv{ind}.completion.durationSec = 2.95; % 3 seconds
 mission.bhv{ind}.completion.status = false;     % completion flag
 
+%Behavior 4 (mission.bhv{4})
 ind = ind+1;
 mission.bhv{ind}.name = 'bhv_land';
 mission.bhv{ind}.params.maxDescentRateMps = 0.2;
